3723d0b1e1b5acf40d1687d6f6fca9a8cbbb6d18
bpshonyak <bpshonyak@live.com>
2015-12-05 15:03:49 -0800

    shit works

--- hack/www/com/student/student.js
+++ hack/www/com/student/student.js
@@ -1,38 +1,19 @@
 'use strict()';
 
-function StudentCtrl($scope) {
+function StudentCtrl($scope, SocketServ, StudentServ) {
   console.log('StudentCtrl');
-  $scope.teams = [
-    {
-      id: 'mu',
-      icon: 'img/',
-      question: 'When was WWI started?',
-      value: 1,
-      score: 0
-    }, {
-      id: 'rm',
-      icon: 'img/',
-      question: 'What is the capital of Oregon?',
-      value: 2,
-      score: 0
-    },
-    {
-      id: 'se',
-      icon: 'img/',
-      question: 'How old is Obama?',
-      value: 2,
-      score: 0
-    },
-    {
-      id: 'xd',
-      icon: 'img/',
-      question: 'None of the above',
-      value: 2,
-      score: 0
-    }
-  ];
+
+  SocketServ.on("Student Receives", function (data) {
+    console.log("Recived: " + data);
+  });
+
+  // SocketServ.on("Teacher Asks", function (data) {
+  //   console.log("Recived: " + data);
+  // });
 
   $scope.data = {};
+
+  $scope.teams = StudentServ.getAll();
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
--- hack/www/fac/student.js
+++ hack/www/fac/student.js
@@ -1,13 +1,37 @@
-function StudentServ ($scope, SocketServ, QuestionServ) {
+'use strict()';
+
+function StudentServ () {
   console.log('StudentServ');
 
-  SocketServ.on("Student Receives", function (data) {
-    console.log("Recived: " + data);
-  });
-
-  var students = [{
-
-  }];
+  var students = [
+    {
+      id: 'mu',
+      icon: 'img/',
+      question: 'When was WWI started?',
+      value: 1,
+      score: 0
+    }, {
+      id: 'rm',
+      icon: 'img/',
+      question: 'What is the capital of Oregon?',
+      value: 2,
+      score: 0
+    },
+    {
+      id: 'se',
+      icon: 'img/',
+      question: 'How old is Obama?',
+      value: 2,
+      score: 0
+    },
+    {
+      id: 'xd',
+      icon: 'img/',
+      question: 'None of the above',
+      value: 2,
+      score: 0
+    }
+  ];
 
   return {
     getAll : function () {
