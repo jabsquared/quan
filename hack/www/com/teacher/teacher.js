32b8f9c895c7aa85ee00b556562847563094ce49
bpshonyak <bpshonyak@live.com>
2015-12-05 11:46:03 -0800

    started question queue. question now in scope var.

--- hack/www/com/teacher/teacher.js
+++ hack/www/com/teacher/teacher.js
@@ -2,7 +2,32 @@
 
 function TeacherCtrl($scope) {
   console.log('TeacherCtrl');
-
+  $scope.questions = [
+    {
+      question: "When was WWI started?",
+      category: "History"
+    },
+    {
+      question: "What is the capital of Oregon?",
+      category: "Geography",
+      answers: [
+        "1992",
+        "1942",
+        "1893",
+        "1953"
+      ]
+    },
+    {
+      question: "How old is Obama?",
+      category: "Trivia",
+      answers: [
+        "45",
+        "39",
+        "51",
+        "41"
+      ]
+    }
+  ];
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
