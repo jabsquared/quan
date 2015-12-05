26e18b29f9dca486b202988ab70089d554e6aa35
Yonas Berhe <sanoy@Yonass-MacBook-Air.local>
2015-12-05 12:29:02 -0800

    added json to radios

--- hack/www/com/student/student.js
+++ hack/www/com/student/student.js
@@ -1,33 +1,3 @@
-// 'use strict';
-//
-// function StudentCtrl($scope) {
-//   console.log('StudentCtrl');
-//   $scope.teams = [
-//     {
-//       id: 'mu',
-//       icon: 'img/',
-//       name: 'Man United',
-//       value: 1,
-//       score: 0
-//     }, {
-//       id: 'rm',
-//       icon: 'img/',
-//       name: 'Real Mad',
-//       value: 2,
-//       score: 0
-//     }
-//   ];
-//   // With the new view caching in Ionic, Controllers are only called
-//   // when they are recreated or on app start, instead of every page change.
-//   // To listen for when this page is active (for example, to refresh data),
-//   // listen for the $ionicView.enter event:
-//   //$scope.$on('$ionicView.enter', function(e) {
-//   //});
-//
-// }
-
-
-
 'use strict';
 
 function StudentCtrl($scope) {
