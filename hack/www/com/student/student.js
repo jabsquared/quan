837e26d0033c103e710aa8aa0f17a4676ce95ed3
bpshonyak <bpshonyak@live.com>
2015-12-05 15:25:12 -0800

    broken student ui

--- hack/www/com/student/student.js
+++ hack/www/com/student/student.js
@@ -3,17 +3,20 @@
 function StudentCtrl($scope, SocketServ, StudentServ) {
   console.log('StudentCtrl');
 
+  $scope.data = {
+    
+  };
+  $scope.teams = StudentServ.getAll();
+
   SocketServ.on("Student Receives", function (data) {
-    console.log("Recived: " + data);
+    console.log(data);
+    $scope.data = data;
   });
 
   // SocketServ.on("Teacher Asks", function (data) {
   //   console.log("Recived: " + data);
   // });
 
-  $scope.data = {};
-
-  $scope.teams = StudentServ.getAll();
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
