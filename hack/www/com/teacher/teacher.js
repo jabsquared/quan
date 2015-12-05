07b05b60970b25e9f71edec7bccd0b1b6cc0c343
bpshonyak <bpshonyak@live.com>
2015-12-05 14:10:31 -0800

    added emits and ons and shit.

--- hack/www/com/teacher/teacher.js
+++ hack/www/com/teacher/teacher.js
@@ -1,64 +1,12 @@
 'use strict()';
 
-function TeacherCtrl($scope, SocketServ) {
+function TeacherCtrl($scope, SocketServ, QuestionServ) {
   console.log('TeacherCtrl');
 
   $scope.pushQuestion = function (q) {
     console.log("Question (" + q.question + ") Pushed!");
+    SocketServ.emit('Ask Question', q);
   };
 
-  $scope.questions = [
-    {
-      question: "What are you less likely to leave home without?",
-      category: "NONE",
-      answers: [
-        "Keys",
-        "Wallet",
-        "Phone",
-        "ID"
-      ]
-    },
-    {
-      question: "What is the the most common use of the phone?",
-      category: "NONE",
-      answers: [
-        "Text",
-        "Social Media",
-        "Web",
-        "Games"
-      ]
-    },
-    {
-      question: "What do teachers fear the most?",
-      category: "NONE",
-      answers: [
-        "SnapChat",
-        "Instagram",
-        "Yoga",
-        "Phone"
-      ]
-    },
-    {
-      question: "What is the teacher's greatest challenge in a classroom?",
-      category: "NONE",
-      answers: [
-        "Interaction",
-        "Budget",
-        "Class Size",
-        "Tests"
-      ]
-    },
-    {
-      question: "What other tools have been used that haven’t worked?",
-      category: "NONE",
-      answers: [
-        "Chromebook",
-        "Smartboard",
-        "Pencil",
-        "Clicker",
-        "Phone"
-      ]
-    }
-  ];
-
+  $scope.questions = QuestionServ.getAll();
 }
--- hack/www/fac/question.js
+++ hack/www/fac/question.js
@@ -1,9 +1,59 @@
 function QuestionServ () {
   console.log('QuestionServ');
 
-  var questions = [{
-
-  }]
+  var questions = [
+    {
+      question: "What are you less likely to leave home without?",
+      category: "NONE",
+      answers: [
+        "Keys",
+        "Wallet",
+        "Phone",
+        "ID"
+      ]
+    },
+    {
+      question: "What is the the most common use of the phone?",
+      category: "NONE",
+      answers: [
+        "Text",
+        "Social Media",
+        "Web",
+        "Games"
+      ]
+    },
+    {
+      question: "What do teachers fear the most?",
+      category: "NONE",
+      answers: [
+        "SnapChat",
+        "Instagram",
+        "Yoga",
+        "Phone"
+      ]
+    },
+    {
+      question: "What is the teacher's greatest challenge in a classroom?",
+      category: "NONE",
+      answers: [
+        "Interaction",
+        "Budget",
+        "Class Size",
+        "Tests"
+      ]
+    },
+    {
+      question: "What other tools have been used that haven’t worked?",
+      category: "NONE",
+      answers: [
+        "Chromebook",
+        "Smartboard",
+        "Pencil",
+        "Clicker",
+        "Phone"
+      ]
+    }
+  ];
 
   return {
     getAll : function () {
