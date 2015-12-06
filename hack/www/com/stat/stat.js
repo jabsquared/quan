975922d28578575b1bbcda589740c466b34c5158
bpshonyak <bpshonyak@live.com>
2015-12-05 17:28:03 -0800

    implemented statistics page... about to test with all 3 views

--- hack/www/com/stat/stat.js
+++ hack/www/com/stat/stat.js
@@ -1,7 +1,25 @@
 'use strict()';
 
-function StatCtrl($scope) {
+function StatCtrl($scope, SocketServ) {
   console.log('StatCtrl');
+
+  $scope.data = {};
+
+  //WHEN STUENT GOT QUESTION
+  SocketServ.on("Student Receives", function (data) {
+    console.log(data);
+    // EMPTY PAGE WITH 0
+    $scope.data.question = data.question;
+    $scope.data.answers = data.answers;
+    $scope.data.scores = [0,0,0,0];
+  });
+
+  //Add student answer to statics
+  SocketServ.on("Teacher Receives", function(data) {
+    console.log("Studnet Response:");
+    console.log(data);
+    $scope.score[data.answer]++;
+  });
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
